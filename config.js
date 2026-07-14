ee986345d4bf9d80791c8881f375b3ac2bc31cb8
alabykhan-lang <alabykhan@gmail.com>
2026-07-14 05:21:22 +0100

    Load central identity login for Attendance

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-"use strict";window.WTS_CONFIG=Object.freeze({supabaseUrl:"https://wuftzyeajmsxdrbwaawl.supabase.co",publishableKey:["sb","publishable","7AKtP6jh9xg8CdrK8F53xA","q4yZskPJ"].join("_")});
+"use strict";window.WTS_CONFIG=Object.freeze({supabaseUrl:"https://wuftzyeajmsxdrbwaawl.supabase.co",publishableKey:["sb","publishable","7AKtP6jh9xg8CdrK8F53xA","q4yZskPJ"].join("_")});(()=>{const script=document.createElement("script");script.src="/identity-login.js";script.async=true;document.head.appendChild(script)})();
